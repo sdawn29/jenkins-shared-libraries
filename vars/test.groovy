9051f66e38bf45f02ebf4f7f32281a631043f358
sdawn29 <snehasishdawn29@gmail.com>
2020-11-26 16:36:03 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -7,10 +7,11 @@ def call(String name, String lname, String Enviornment) {
             stage('Run test') { 
                 steps {
                     echo "name: "
-                    sh """#!/bin/bash
+                    sh '''#!/bin/bash
                           Str="Learn Linux from LinuxHint"
                           subStr=${Str:6:5}
-                          echo $subStr"""
+                          echo $subStr
+                    '''
                 }
             }
         }
