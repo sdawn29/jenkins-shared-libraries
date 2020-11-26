5721d5bfc8ba3c031008628c0dcebf9111cac0b1
sdawn29 <snehasishdawn29@gmail.com>
2020-11-26 17:24:40 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,10 +1,11 @@
 #!/usr/bin/env groovy
- def shsript = '''#!/bin/bash
+ 
+def call(String name, String lname, String Enviornment) {
+    pipeline {
+     def shsript = '''#!/bin/bash
                     NAME=$1
                     LASTNAME=$2
                     echo "Hello, $NAME $LASTNAME"'''
-def call(String name, String lname, String Enviornment) {
-    pipeline {
         agent any
         stages {
             stage('Run test') { 
